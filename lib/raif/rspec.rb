0d04428f677a7792797d93b484c28fa0ea500f1d
Ben Roesch <ben@cultivatelabs.com>
2025-03-04 11:08:15 -0500

    Remove test client ref

--- lib/raif/rspec.rb
+++ lib/raif/rspec.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require_relative "../../spec/support/test_client"
 require_relative "../../spec/support/rspec/llm_stubbing"
 require_relative "../../spec/support/test_model_tool"
 require_relative "../../spec/support/test_conversation"
