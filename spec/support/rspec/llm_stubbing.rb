75e5e3c6baf2deb2596b12c6d26977ad1213839f
Ben Roesch <ben@cultivatelabs.com>
2025-03-04 20:00:09 -0500

    Fix stubbing completions

--- spec/support/rspec/llm_stubbing.rb
+++ spec/support/rspec/llm_stubbing.rb
@@ -5,9 +5,9 @@ module Raif
     module LlmStubbing
 
       def stub_raif_completion(completion_class, &block)
-        allow_any_instance_of(Raif::Llm).to receive(:chat) do |_instance, messages|
+        allow_any_instance_of(Raif::Llm).to receive(:chat) do |_instance, args|
           {
-            response: block.call(messages),
+            response: block.call(args[:messages]),
             prompt_tokens: rand(1..4),
             completion_tokens: rand(10..30)
           }
