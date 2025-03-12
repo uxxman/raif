8b818c1d839ff30611d576ef5dd764d3c032cf91
Ben Roesch <ben@cultivatelabs.com>
2025-03-12 10:23:50 -0400

    Fix tests

--- spec/dummy/config/initializers/raif.rb
+++ spec/dummy/config/initializers/raif.rb
@@ -4,6 +4,9 @@ Raif.configure do |config|
   # config.conversations_controller = "ConversationsController"
   # config.conversation_entries_controller = "ConversationEntriesController"
 
+  config.anthropic_api_key = "placeholder"
+  config.open_ai_api_key = "placeholder"
+
   config.authorize_controller_action = ->() { true }
   config.authorize_admin_controller_action = ->() { true }
 end
