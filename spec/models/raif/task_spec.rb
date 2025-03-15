f95235e01f71e763d75fb1172032220ade8e2d4b
Ben Roesch <ben@cultivatelabs.com>
2025-03-15 15:07:54 -0400

    Add test coverage for Task.run

--- spec/models/raif/task_spec.rb
+++ spec/models/raif/task_spec.rb
@@ -31,4 +31,83 @@ RSpec.describe Raif::Task, type: :model do
       expect(task.errors[:llm_model_key]).to include("is not included in the list")
     end
   end
+
+  describe ".run" do
+    let(:user) { FB.create(:raif_test_user) }
+    context "for a task requesting a text response" do
+      before do
+        stub_raif_task(Raif::TestTask) do |_messages|
+          "Why is a pirate's favorite letter 'R'? Because, if you think about it, 'R' is the only letter that makes sense."
+        end
+      end
+
+      it "runs the task" do
+        task = Raif::TestTask.run(creator: user)
+        expect(task).to be_persisted
+        expect(task.creator).to eq(user)
+        expect(task.started_at).to be_present
+        expect(task.completed_at).to be_present
+        expect(task.prompt).to eq("Tell me a joke")
+        expect(task.system_prompt).to eq("You are a helpful assistant.\nYou are also good at telling jokes.")
+        expect(task.response_format).to eq("text")
+        expect(task.response).to eq("Why is a pirate's favorite letter 'R'? Because, if you think about it, 'R' is the only letter that makes sense.")
+        expect(task.parsed_response).to eq("Why is a pirate's favorite letter 'R'? Because, if you think about it, 'R' is the only letter that makes sense.") # rubocop:disable Layout/LineLength
+
+        expect(task.raif_model_completion).to be_persisted
+        expect(task.raif_model_completion.source).to eq(task)
+        expect(task.raif_model_completion.raw_response).to eq("Why is a pirate's favorite letter 'R'? Because, if you think about it, 'R' is the only letter that makes sense.") # rubocop:disable Layout/LineLength
+      end
+    end
+
+    context "for a task requesting a JSON response" do
+      before do
+        stub_raif_task(Raif::TestJsonTask) do |_messages|
+          {
+            joke: "Why is a pirate's favorite letter 'R'? Because, if you think about it, 'R' is the only letter that makes sense.",
+            answer: "R"
+          }.to_json
+        end
+      end
+
+      it "runs the task" do
+        task = Raif::TestJsonTask.run(creator: user)
+        expect(task).to be_persisted
+        expect(task.creator).to eq(user)
+        expect(task.started_at).to be_present
+        expect(task.completed_at).to be_present
+        expect(task.prompt).to eq("Tell me a joke")
+        expect(task.system_prompt).to eq("You are a helpful assistant.\nYou are also good at telling jokes. Your response should be a JSON object with the following keys: joke, answer.")
+        expect(task.response_format).to eq("json")
+        expect(task.response).to eq("{\"joke\":\"Why is a pirate's favorite letter 'R'? Because, if you think about it, 'R' is the only letter that makes sense.\",\"answer\":\"R\"}")
+
+        expect(task.raif_model_completion).to be_persisted
+        expect(task.raif_model_completion.source).to eq(task)
+        expect(task.raif_model_completion.raw_response).to eq("{\"joke\":\"Why is a pirate's favorite letter 'R'? Because, if you think about it, 'R' is the only letter that makes sense.\",\"answer\":\"R\"}")
+      end
+    end
+
+    context "for a task requesting an HTML response" do
+      before do
+        stub_raif_task(Raif::TestHtmlTask) do |_messages|
+          "<p>Why is a pirate's favorite letter 'R'?</p><p>Because, if you think about it, <strong>'R'</strong> is the only letter that makes sense.</p>"
+        end
+      end
+
+      it "runs the task" do
+        task = Raif::TestHtmlTask.run(creator: user)
+        expect(task).to be_persisted
+        expect(task.creator).to eq(user)
+        expect(task.started_at).to be_present
+        expect(task.completed_at).to be_present
+        expect(task.prompt).to eq("Tell me a joke")
+        expect(task.system_prompt).to eq("You are a helpful assistant.\nYou are also good at telling jokes. Your response should be an HTML snippet that is formatted with basic HTML tags.")
+        expect(task.response_format).to eq("html")
+        expect(task.response).to eq("<p>Why is a pirate's favorite letter 'R'?</p><p>Because, if you think about it, <strong>'R'</strong> is the only letter that makes sense.</p>")
+
+        expect(task.raif_model_completion).to be_persisted
+        expect(task.raif_model_completion.source).to eq(task)
+        expect(task.raif_model_completion.raw_response).to eq("<p>Why is a pirate's favorite letter 'R'?</p><p>Because, if you think about it, <strong>'R'</strong> is the only letter that makes sense.</p>")
+      end
+    end
+  end
 end
--- spec/support/test_task.rb
+++ spec/support/test_task.rb
@@ -1,4 +1,35 @@
 # frozen_string_literal: true
 
 class Raif::TestTask < Raif::Task
+  def build_prompt
+    "Tell me a joke"
+  end
+
+  def build_system_prompt
+    super + "\nYou are also good at telling jokes."
+  end
+end
+
+class Raif::TestJsonTask < Raif::Task
+  llm_response_format :json
+
+  def build_prompt
+    "Tell me a joke"
+  end
+
+  def build_system_prompt
+    super + "\nYou are also good at telling jokes. Your response should be a JSON object with the following keys: joke, answer."
+  end
+end
+
+class Raif::TestHtmlTask < Raif::Task
+  llm_response_format :html
+
+  def build_prompt
+    "Tell me a joke"
+  end
+
+  def build_system_prompt
+    super + "\nYou are also good at telling jokes. Your response should be an HTML snippet that is formatted with basic HTML tags."
+  end
 end
