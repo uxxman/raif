ce9a372b0229b2ef31266f1ff4020856bf2c4377
Ben Roesch <ben@cultivatelabs.com>
2025-03-20 11:02:46 -0400

    Start adding conversation interface to dummy app for testing

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -11,8 +11,8 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Chat routes
+  get "chat" => "chat#index", as: :chat
 
   # Mount the Raif engine
   mount Raif::Engine, at: "/raif"
