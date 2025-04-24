481ca0b57ba817461a11a7abc11fac90d57ded02
Ben Roesch <ben@cultivatelabs.com>
2025-04-24 16:43:14 -0400

    Add per task cost

--- app/controllers/raif/admin/stats/tasks_controller.rb
+++ app/controllers/raif/admin/stats/tasks_controller.rb
@@ -9,6 +9,15 @@ module Raif
           @time_range = get_time_range(@selected_period)
 
           @task_count = Raif::Task.where(created_at: @time_range).count
+
+          # Get task counts by type
+          @task_counts_by_type = Raif::Task.where(created_at: @time_range).group(:type).count
+
+          # Get costs by task type
+          @task_costs_by_type = Raif::Task.joins(:raif_model_completion)
+            .where(created_at: @time_range)
+            .group(:type)
+            .sum("raif_model_completions.total_cost")
         end
       end
     end
