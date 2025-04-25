598aaa975690323b873f2f5eff90cc86277c7e62
Ben Roesch <ben@cultivatelabs.com>
2025-04-25 11:26:57 -0400

    Enable models in dummy app for testing

--- spec/dummy/config/initializers/raif.rb
+++ spec/dummy/config/initializers/raif.rb
@@ -5,7 +5,14 @@ Raif.configure do |config|
   # config.conversation_entries_controller = "ConversationEntriesController"
 
   config.anthropic_api_key = "placeholder"
+  config.anthropic_models_enabled = true
+
   config.open_ai_api_key = "placeholder"
+  config.open_ai_models_enabled = true
+  config.open_ai_embedding_models_enabled = true
+
+  config.open_router_api_key = "placeholder"
+  config.open_router_models_enabled = true
 
   config.authorize_controller_action = ->() { true }
   config.authorize_admin_controller_action = ->() { true }
